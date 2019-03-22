33b3116e72cfad78ff2c57497abbf1e6dc828411
viclafouch <victor.dlf@outlook.fr>
2019-03-22 23:15:00 +0100

    eslint implementation

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,14 +1,15 @@
-import React,  { Component }  from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+
 export class Popup extends Component {
+  state = {
+    test: 'ok'
+  }
+
   render() {
-    return (
-      <div>
-        Hello
-      </div>
-    )
+    const { test } = this.state
+    return <div>{test}</div>
   }
 }
-
 
 ReactDOM.render(<Popup />, document.getElementById('popup'))
