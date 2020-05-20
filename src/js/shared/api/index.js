c8a0a40cfcd454c9a6222ebf2794b36ade461ff3
viclafouch <victor.dlf@outlook.fr>
2020-05-20 19:26:58 +0200

    revert: API url

--- src/js/shared/api/index.js
+++ src/js/shared/api/index.js
@@ -1,6 +1,6 @@
 import { isDev } from '@utils/utils'
 
-const apiUrl = new URL(!isDev ? 'http://localhost:3000' : 'https://crawler.victor-de-la-fouchardiere.fr')
+const apiUrl = new URL(isDev ? 'http://localhost:3000' : 'https://crawler.victor-de-la-fouchardiere.fr')
 
 export const fetchCrawler = (url, params) => {
   if (typeof url === 'string' && url.startsWith('/')) {
